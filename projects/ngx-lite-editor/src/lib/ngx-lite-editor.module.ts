673ab64b9c2cb650198785e90540d73fb925b9a4
kain <kainpro@foxmail.com>
2019-04-02 18:40:55 +0800

    Submit

--- projects/ngx-lite-editor/src/lib/ngx-lite-editor.module.ts
+++ projects/ngx-lite-editor/src/lib/ngx-lite-editor.module.ts
@@ -1,11 +1,19 @@
 import {NgModule} from '@angular/core';
 import {NgxLiteEditorComponent} from './ngx-lite-editor.component';
 import {CommonModule} from '@angular/common';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [NgxLiteEditorComponent],
-  exports: [NgxLiteEditorComponent]
+  imports: [
+    CommonModule,
+    NgZorroAntdModule
+  ],
+  declarations: [
+    NgxLiteEditorComponent
+  ],
+  exports: [
+    NgxLiteEditorComponent
+  ]
 })
 export class NgxLiteEditorModule {
 }
