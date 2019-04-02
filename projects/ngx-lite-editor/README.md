563efcae5e651f24917f3b465e7520dadfb113a4
kain <zhangtqx@vip.qq.com>
2019-04-02 17:10:32 +0800

    Submit

---
+++ projects/ngx-lite-editor/README.md
@@ -0,0 +1,1 @@
+# Ngx-Lite-Editor
---
+++ projects/ngx-lite-editor/src/lib/ngx-lite-editor.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+import {NgxLiteEditorComponent} from './ngx-lite-editor.component';
+
+@NgModule({
+  declarations: [NgxLiteEditorComponent],
+  imports: [],
+  exports: [NgxLiteEditorComponent]
+})
+export class NgxLiteEditorModule {
+}
