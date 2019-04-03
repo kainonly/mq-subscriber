452efe6345d6079d4938d0845da1ae3297c2a33f
kain <zhangtqx@vip.qq.com>
2019-04-03 17:52:54 +0800

    Submit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  html = 'asd';
 
+  htmlChange(event) {
+    console.log(event);
+  }
 }
