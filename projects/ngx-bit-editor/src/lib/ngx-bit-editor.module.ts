2923943acbbca94432d2e5801521c53a420cb898
kain <kainpro@foxmail.com>
2019-04-03 15:32:20 +0800

    Submit

---
+++ projects/ngx-bit-editor/src/lib/ngx-bit-editor.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {NgxBitEditorComponent} from './ngx-bit-editor.component';
+
+@NgModule({
+  imports: [
+    FormsModule
+  ],
+  declarations: [NgxBitEditorComponent],
+  exports: [NgxBitEditorComponent]
+})
+export class NgxBitEditorModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import {Component} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('htmlTextAreaElement') htmlTextAreaElement: ElementRef;
+
+  ngAfterViewInit() {
+    console.log(this.htmlTextAreaElement.nativeElement);
+  }
 }
