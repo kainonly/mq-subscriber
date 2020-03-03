803758ed15a5424bd4488cf720a9507321b0c530
kain <zhangtqx@vip.qq.com>
2020-03-03 17:00:16 +0800

    更新

--- trigger/put.go
+++ trigger/put.go
@@ -1,5 +1,25 @@
 package trigger
 
-func (c *Trigger) Put() (err error) {
+import "sync"
+
+func (c *Trigger) Put(identity string, queue string) (err error) {
+	delivery, err := c.channel.Consume(
+		queue,
+		"",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for d := range delivery {
+			println(d.Body)
+		}
+	}()
+	wg.Wait()
 	return
 }
--- trigger/trigger.go
+++ trigger/trigger.go
@@ -5,3 +5,9 @@ import "github.com/streadway/amqp"
 type Trigger struct {
 	channel *amqp.Channel
 }
+
+func Inject(channel *amqp.Channel) *Trigger {
+	trigger := new(Trigger)
+	trigger.channel = channel
+	return trigger
+}
