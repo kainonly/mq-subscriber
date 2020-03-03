ab2e97618318df49341e57b2e76fb72f7bd81dff
kain <kainpro@foxmail.com>
2020-03-03 17:00:16 +0800

    更新

--- main.go
+++ main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"amqp-subscriber/client"
+	"amqp-subscriber/trigger"
 	"gopkg.in/ini.v1"
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
+	go func() {
+		http.ListenAndServe(":6060", nil)
+	}()
 	cfg, err := ini.Load("config.ini")
 	if err != nil {
 		log.Fatalln(err)
 	}
+	// TODO: GRPC Server 初始化...
+
 	conn, err := client.NewClient(cfg.Section("AMQP"))
 	if err != nil {
 		log.Fatalln(err)
@@ -21,4 +29,5 @@ func main() {
 		log.Fatal(err)
 	}
 	defer channel.Close()
+	trigger.Inject(channel)
 }
