2a5c97c0e6d3f957add31a20697d6f1c0d4b10cf
kain <kainpro@foxmail.com>
2020-03-04 12:06:01 +0800

    增加配置

--- controller/put.go
+++ controller/put.go
@@ -1,12 +1,18 @@
 package controller
 
 import (
+	"amqp-subscriber/common"
 	pb "amqp-subscriber/router"
 	"context"
 )
 
 func (c *controller) Put(ctx context.Context, query *pb.PutParameter) (*pb.Response, error) {
-	err := c.subscribe.Put(query.Identity, query.Queue)
+	err := c.subscribe.Put(common.SubscriberOption{
+		Identity: query.Identity,
+		Queue:    query.Queue,
+		Url:      query.Url,
+		Secret:   query.Secret,
+	})
 	if err != nil {
 		return &pb.Response{
 			Error: 1,
