8eebfca651d4d690ea9171ddf2f0550c82f5dc5b
kain <zhangtqx@vip.qq.com>
2020-03-15 10:28:48 +0800

    更新Tag自动构建

--- scripts/ci-tag.sh
+++ scripts/ci-tag.sh
@@ -2,8 +2,8 @@
 # Login docker
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 # Build Golang Application
-go build -o dist/ssh-microservice
+go build -o dist/amqp-subscriber
 # Build docker image
-docker build . -t kainonly/ssh-microservice:${TRAVIS_TAG}
+docker build . -t kainonly/amqp-subscriber:${TRAVIS_TAG}
 # Push docker image
-docker push kainonly/ssh-microservice:${TRAVIS_TAG}
+docker push kainonly/amqp-subscriber:${TRAVIS_TAG}
