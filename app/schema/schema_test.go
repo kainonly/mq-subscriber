0c4be4eeb82b027823ce5cc403e70643568edbfe
kain <kainpro@foxmail.com>
2020-09-02 23:48:00 +0800

    update schema test

---
+++ app/schema/schema_test.go
@@ -0,0 +1,41 @@
+package schema
+
+import (
+	"amqp-subscriber/app/types"
+	"os"
+	"testing"
+)
+
+var schema *Schema
+
+func TestMain(m *testing.M) {
+	os.Chdir("../..")
+	schema = New()
+	os.Exit(m.Run())
+}
+
+func TestSchema_Update(t *testing.T) {
+	err := schema.Update(types.SubscriberOption{
+		Identity: "task",
+		Queue:    "test",
+		Url:      "http://localhost:3000",
+		Secret:   "fq7K8EsCMjrv4wOB",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSchema_Lists(t *testing.T) {
+	_, err := schema.Lists()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSchema_Delete(t *testing.T) {
+	err := schema.Delete("task")
+	if err != nil {
+		t.Fatal(err)
+	}
+}
