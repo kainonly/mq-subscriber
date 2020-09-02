5b142ef41b87fafd9cb0b028391d802d55bc0389
kain <kainpro@foxmail.com>
2020-09-02 21:12:02 +0800

    reset

--- scripts/ci-default.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Login docker
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-# Build Golang Application
-go build -o dist/amqp-subscriber
-# Build docker image
-docker build . -t kainonly/amqp-subscriber:latest
-# Push docker image
-docker push kainonly/amqp-subscriber:latest
